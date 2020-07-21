c21ddbc9e7603644630506167711f844c9164378
Jack Wong <jackwong3101@yahoo.com>
2020-07-21 19:15:10 +0800

    Ready for deployment to heroku

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/db");
+const path = require("path"); // required in production environment
 
 // Connect database
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send("API Runnning");
-});
 
 app.use(express.json({ extended: false }));
 
@@ -18,6 +15,15 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("./client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}. `);
