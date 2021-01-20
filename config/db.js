40384a5b3b8f3de68a3d3f7fbb113db94fd7c02b
Jack Wong <jackwong3101@yahoo.com>
2021-01-21 01:35:06 +0800

    Pass process.env.MONGO_URI directly instead of using a const

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = process.env.MONGO_URI
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,65 +1,65 @@
-const express = require("express");
-const router = express.Router();
-const { check, validationResult } = require("express-validator");
-const User = require("../../models/User");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const bcrypt = require("bcryptjs");
-const auth = require("../../middleware/auth");
+const express = require('express')
+const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const User = require('../../models/User')
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const auth = require('../../middleware/auth')
 
 // @route GET /api/auth
 // @desc Get user info + Authenticate with existing token
 // access Private
-router.get("/", auth, async (req, res) => {
+router.get('/', auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select("-password").exec();
-    console.log(user);
-    res.json(user);
+    const user = await User.findById(req.user.id).select('-password').exec()
+    console.log(user)
+    res.json(user)
   } catch (e) {}
-});
+})
 
 // @route    POST /api/auth
 // @desc     Authenticate and get token
 // @access   Public
 const validator = [
-  check("email", "Please enter a valid email address").isEmail(),
-  check("password", "Password is required").exists()
-];
-router.post("/", [...validator], async (req, res) => {
-  const errors = validationResult(req);
+  check('email', 'Please enter a valid email address').isEmail(),
+  check('password', 'Password is required').exists()
+]
+router.post('/', [...validator], async (req, res) => {
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() })
   }
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   try {
     // See if user exists
-    let user = await User.findOne({ email });
+    let user = await User.findOne({ email })
     if (!user) {
-      return res.status(400).json({ errors: [{ msg: "Invalid credential" }] }); // Be careful not to say email not found, as we don want people to find one if some email goes through
+      return res.status(400).json({ errors: [{ msg: 'Invalid credential' }] }) // Be careful not to say email not found, as we don want people to find one if some email goes through
     }
     // Check if password is correct
-    const isMatch = bcrypt.compareSync(password, user.password);
+    const isMatch = bcrypt.compareSync(password, user.password)
     if (!isMatch) {
-      return res.status(400).json({ errors: [{ msg: "Invalid credential" }] });
+      return res.status(400).json({ errors: [{ msg: 'Invalid credential' }] })
     }
     // Create and return jsonwebtoken
-    const key = process.env.JWT_SECRET);
+    const key = process.env.JWT_SECRET
     const payload = {
       user: {
         id: user.id
       }
-    };
-    jwt.sign(payload, key, { expiresIn: "2 days" }, (err, encoded) => {
-      if (err) throw err;
+    }
+    jwt.sign(payload, key, { expiresIn: '2 days' }, (err, encoded) => {
+      if (err) throw err
       res.json({
         token: encoded
-      });
-    });
+      })
+    })
   } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ msg: "server error" });
+    console.log(err.message)
+    res.status(500).json({ msg: 'server error' })
   }
-});
+})
 
-module.exports = router;
+module.exports = router
